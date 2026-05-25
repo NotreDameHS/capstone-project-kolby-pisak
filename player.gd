9912b3ad82de216ed7aff27d0876963164c4a315
KolbyPisak <kolbypisak@gmail.com>
2026-05-25 11:24:06 -0600

    Started added weapons.

--- player.gd
+++ player.gd
@@ -1,35 +1,16 @@
 extends Area2D
+@export var max_speed := 600.0
+@export var health := 100
 var velocity := Vector2(0, 0)
-var max_speed := normal_speed
-@export var normal_speed := 200.0
-@export var boost_speed := 500.0
-@export var steering_factor := 1.0
 
 func _process(delta: float) -> void:
 	var direction := Vector2(0, 0)
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
-	
+
+	# If the vector is longer than 1 (diagonal), shrink it back to 1
 	if direction.length() > 1.0:
 		direction = direction.normalized()
-		
-	if velocity.length() > 0.0:
-		# Instead of rotating the whole ship, we only rotate the Sprite2D
-		get_node("Sprite2D").rotation = velocity.angle()
-		
-	if Input.is_action_just_pressed("boost"):
-		max_speed = boost_speed
-		get_node("Timer").start()
-	
-	var desired_velocity := direction * max_speed
-	var steering_vector := desired_velocity - velocity
-	
-	velocity += steering_vector * steering_factor * delta
+
+	velocity = direction * max_speed
 	position += velocity * delta
-	
-	var viewport_size := get_viewport_rect().size
-	position.x = wrapf(position.x, 0, viewport_size.x)
-	position.y = wrapf(position.y, 0, viewport_size.y)
-	
-func _on_timer_timeout() -> void:
-	max_speed = normal_speed
