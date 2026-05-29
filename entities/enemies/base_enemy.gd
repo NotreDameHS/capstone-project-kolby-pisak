25f5102f38ae18d0349e90324f3d2c56c88a1344
KolbyPisak <kolbypisak@gmail.com>
2026-05-29 11:21:00 -0600

    Gave Enemy Health and Made Bullets Subtract Health

--- entities/enemies/base_enemy.gd
+++ entities/enemies/base_enemy.gd
@@ -6,10 +6,24 @@ var target_pos
 @export var SPEED := 300.0
 @export var health := 25
 @onready var player = get_parent().get_node("Weapon")
+@onready var health_bar := $HealthBar
+
+func _ready() -> void:
+	health_bar.max_value = health
+	health_bar.value = health
 
 func _physics_process(delta: float) -> void:
+	#Sets variables here so that they can update every tick
 	player_pos = player.position 
 	target_pos = (player_pos - position).normalized()
 	
 	if position.distance_to(player_pos) > 3:
 		position += target_pos * SPEED * delta
+
+func take_damage(damage) -> void:
+	health -= damage
+	
+	if health <= 0:
+		queue_free()
+		
+	health_bar.value -= damage
--- entities/weapons/bullet.gd
+++ entities/weapons/bullet.gd
@@ -1,10 +1,13 @@
 extends Sprite2D
 
 @onready var RayCast: RayCast2D = $RayCast2D
-
+@export var damage = 13
 var speed: float = 1000
 
 func _physics_process(delta: float) -> void:
 	global_position += Vector2(1,0).rotated(rotation) * speed * delta
 	if RayCast.is_colliding():
+		var target = RayCast.get_collider()
+		target.take_damage(damage)
 		queue_free()
+		
