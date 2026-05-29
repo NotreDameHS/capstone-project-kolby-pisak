7ecea199aca4d5fed100b52ce573c43980ba7f93
KolbyPisak <kolbypisak@gmail.com>
2026-05-29 10:53:13 -0600

    Finished Enemy Movement Code

--- entities/enemies/base_enemy.gd
+++ entities/enemies/base_enemy.gd
@@ -1,18 +1,15 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-
+var player_pos
+var target_pos
+@export var SPEED := 300.0
+@export var health := 25
+@onready var player = get_parent().get_node("Weapon")
 
 func _physics_process(delta: float) -> void:
-
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	player_pos = player.position 
+	target_pos = (player_pos - position).normalized()
+	
+	if position.distance_to(player_pos) > 3:
+		position += target_pos * SPEED * delta
